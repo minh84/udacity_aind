977bc822638cbfa58222db2049deb5d0db2306aa
Minh VU <ngoc.vu@polytechnique.org>
2017-11-13 22:41:29 +0000

    wip AIND2-RNN

--- AIND_T2P2-RNN/my_answers.py
+++ AIND_T2P2-RNN/my_answers.py
@@ -13,6 +13,10 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
+    for i in range(len(series) - window_size):
+        X.append(series[i:i+window_size])
+        y.append(series[i+window_size])
+
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,13 +27,19 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
-
+    rnn = Sequential()
+    rnn.add(LSTM(5, input_shape=(window_size, 1)))
+    rnn.add(Dense(1))
+    return rnn
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    text = text.lower()
+    unique_char = set(text)
+    for c in unique_char:
+        if c not in punctuation and (c < 'a' or c > 'z'):
+            text = text.replace(c, ' ')
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -37,7 +47,9 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-
+    for i in range(0, len(text) - window_size, step_size):
+        inputs.append(text[i:i+window_size])
+        outputs.append(text[i+window_size])
     return inputs,outputs
 
 # TODO build the required RNN model: 
